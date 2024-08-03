9eb362d331104a12a8f1147391c0032b21ac8cbd
Ela <98597693+ElaGitNET@users.noreply.github.com>
2024-08-03 04:32:13 +0100

    Add files via upload

---
+++ json/jsonOps.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <fstream>
+
+#include "json.hpp"
+
+#include "../util/utilFunctions.hpp"
+
+namespace json {
+	std::wstring clientDLLFile = L"\\client.dll.json";
+	std::wstring configFile = L"\\config.json";
+	std::wstring offsetFile = L"\\offsets.json";
+	std::wstring buttonsFile = L"\\buttons.json";
+
+	nlohmann::json readFromJsonFile(std::wstring fileName);
+}
